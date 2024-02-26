edbfbe9047421e42ef257d980dc35bc0908a0076
AiSY-Yang <978854518@qq.com>
2024-02-26 11:17:18 +0800

    更新依赖

---
+++ ExtensionMethods.EntityFrameworkCore/EntityEntryExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+#if NETCOREAPP3_0_OR_GREATER
+namespace ExtensionMethods.EntityFrameworkCore
+{
+	public static class EntityEntryExtension
+	{
+		public static void Detached(this IEnumerable<Microsoft.EntityFrameworkCore.ChangeTracking.EntityEntry> entities)
+		{
+			entities.ToList().ForEach(e => e.State = Microsoft.EntityFrameworkCore.EntityState.Detached);
+		}
+	}
+}
+
+#endif
